72478c7e5ddbc519d3b52449fbf23e72d5e86942
Shaw tan <yannshawtan2002@gmail.com>
2023-09-18 22:48:35 +0800

    hi

--- src/oodjassignment/OODJassignment.java
+++ src/oodjassignment/OODJassignment.java
@@ -12,9 +12,10 @@ public class OODJassignment {
 
     /**
      * @param args the command line arguments
+     * 
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        system.out.println("Hello world")
     }
     
 }
