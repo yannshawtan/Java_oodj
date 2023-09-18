1c494f4d7b0c1a7134998f56c9986862c49fa3f0
Shaw tan <yannshawtan2002@gmail.com>
2023-09-18 23:04:58 +0800

    second commit for test

--- src/oodjassignment/OODJassignment.java
+++ src/oodjassignment/OODJassignment.java
@@ -14,7 +14,7 @@ public class OODJassignment {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        system.Out.println("Hello World2");
     }
     
 }
