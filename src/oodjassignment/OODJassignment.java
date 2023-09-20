ab272634f3a09e183d88391993cb285e1e77e526
yannshawtan <125524681+yannshawtan@users.noreply.github.com>
2023-09-20 11:13:55 +0800

    Update OODJassignment.java

--- src/oodjassignment/OODJassignment.java
+++ src/oodjassignment/OODJassignment.java
@@ -14,7 +14,7 @@ public class OODJassignment {
      * @param args the command line arguments
      * 
      */
-    public static void main(String[] args) 
+    public static void main(String[] args) {
         system.Out.println("Hello World2");
 
     }
