9175bb9df47ac9c4298feca59760a1e8e56eec96
Shaw Tan <yannshawtan2002@gmail.com>
2023-10-20 15:17:58 +0800

    edited

---
+++ src/oodjassignment/Customer_Func.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oodjassignment;
+
+
+import javax.swing.DefaultListModel;
+
+
+/**
+ *
+ * @author myste
+ */
+public class Customer_Func {
+    private void loadVendorIDs(DefaultListModel<String> model) {
+        
+    }
+}
