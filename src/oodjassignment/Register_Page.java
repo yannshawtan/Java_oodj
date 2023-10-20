8cebe18944443679d2de9137012ab094cdc4d783
Zorus <samuel518800@gmail.com>
2023-10-20 15:54:46 +0800

    Some progress for admin

---
+++ src/oodjassignment/Register_Page.java
@@ -0,0 +1,343 @@
+package oodjassignment;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Register_Page extends javax.swing.JFrame {
+
+    
+    User owner = new User();
+    
+    public Register_Page() {
+        initComponents();
+    }
+    
+    public Register_Page(String User) {
+        initComponents();
+        this.owner = owner;
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jName = new javax.swing.JLabel();
+        jEmail = new javax.swing.JLabel();
+        jPassword = new javax.swing.JLabel();
+        jConPassword = new javax.swing.JLabel();
+        jTextName = new javax.swing.JTextField();
+        jTextEmail = new javax.swing.JTextField();
+        jTextPassword = new javax.swing.JPasswordField();
+        jTextConPassword = new javax.swing.JPasswordField();
+        jBConfirm = new javax.swing.JButton();
+        jBReset = new javax.swing.JButton();
+        jShowPassword = new javax.swing.JCheckBox();
+        jShowConPassword = new javax.swing.JCheckBox();
+        Role = new javax.swing.JComboBox<>();
+        jBReturn = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        jLabel1.setText("Register Page");
+        jLabel1.setToolTipText("");
+
+        jName.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jName.setText("Name:");
+
+        jEmail.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jEmail.setText("Email:");
+
+        jPassword.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jPassword.setText("Password:");
+
+        jConPassword.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jConPassword.setText("Confirm Password:");
+
+        jTextName.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextNameActionPerformed(evt);
+            }
+        });
+
+        jTextEmail.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextEmailActionPerformed(evt);
+            }
+        });
+
+        jBConfirm.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+        jBConfirm.setText("Confirm");
+        jBConfirm.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jBConfirm.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBConfirmActionPerformed(evt);
+            }
+        });
+
+        jBReset.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+        jBReset.setText("Reset");
+        jBReset.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jBReset.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBResetActionPerformed(evt);
+            }
+        });
+
+        jShowPassword.setText("Show Password");
+        jShowPassword.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jShowPasswordActionPerformed(evt);
+            }
+        });
+
+        jShowConPassword.setText("Show Password");
+        jShowConPassword.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jShowConPasswordActionPerformed(evt);
+            }
+        });
+
+        Role.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Customer", "Vendor", "Runner", "Admin" }));
+        Role.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RoleActionPerformed(evt);
+            }
+        });
+
+        jBReturn.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+        jBReturn.setText("Return");
+        jBReturn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jBReturn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBReturnActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(Role, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(241, 241, 241))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(42, 42, 42)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(8, 8, 8)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jShowPassword)
+                            .addComponent(jShowConPassword)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jName)
+                                    .addComponent(jEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jPassword)
+                                    .addComponent(jConPassword))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                            .addComponent(jTextConPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                                .addComponent(jTextEmail, javax.swing.GroupLayout.Alignment.LEADING)
+                                                .addComponent(jTextPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                        .addComponent(jTextName, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE))))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jBReturn)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jBReset)
+                        .addGap(36, 36, 36)
+                        .addComponent(jBConfirm)))
+                .addContainerGap(48, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(Role, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jName)
+                    .addComponent(jTextName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jEmail)
+                    .addComponent(jTextEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jPassword)
+                    .addComponent(jTextPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jShowPassword)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextConPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jConPassword))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jShowConPassword)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jBConfirm)
+                    .addComponent(jBReturn)
+                    .addComponent(jBReset))
+                .addGap(58, 58, 58))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    public void Return(){
+        Admin_Home_Page hp = new Admin_Home_Page();
+        hp.setVisible(true);
+        hp.pack();
+        hp.setLocationRelativeTo(null);
+        hp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.dispose();
+    }
+
+    private void jTextEmailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextEmailActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextEmailActionPerformed
+
+    private void jBConfirmActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBConfirmActionPerformed
+        
+        String Name = jTextName.getText();
+        String Email = jTextEmail.getText();
+        String Password = jTextPassword.getText();
+        String ConPassword = jTextConPassword.getText();
+        try{
+            if (Name.equals("")){
+                JOptionPane.showMessageDialog(null, "Please fill in your name!");
+            }
+            else if (Email.equals("") || !Email.contains("@")){
+                JOptionPane.showMessageDialog(null, "Please insert a valid email!");
+            }
+            else if (Password.equals("")){
+                JOptionPane.showMessageDialog(null, "Please fill in the password!");
+            }
+            else if (ConPassword.equals("")){
+                JOptionPane.showMessageDialog(null, "Please confirm your password!");
+            }
+            else if (!Password.equals(ConPassword)){
+                JOptionPane.showMessageDialog(null, "Your password doesn't match!");
+            }
+            else{
+                String Position = Role.getSelectedItem().toString();
+                Account_Database db = new Account_Database(Position);
+                if (db.RegisterValidation(Name, Email, Position)){
+                    JOptionPane.showMessageDialog(null, "The name or email given is already been use!");
+                }
+                else{
+                    db.AddData(Name, Email, Password,Position);
+                    Return();
+                }
+            }
+        }
+        catch (NumberFormatException ex){
+            JOptionPane.showMessageDialog(null, "Please enter a valid phone number!");
+        }
+    }//GEN-LAST:event_jBConfirmActionPerformed
+
+    private void jTextNameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextNameActionPerformed
+
+    }//GEN-LAST:event_jTextNameActionPerformed
+
+    private void jShowPasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jShowPasswordActionPerformed
+        if (jShowPassword.isSelected()){
+            jTextPassword.setEchoChar((char)0);
+        }
+        else{
+            jTextPassword.setEchoChar('*');
+        }
+    }//GEN-LAST:event_jShowPasswordActionPerformed
+
+    private void jShowConPasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jShowConPasswordActionPerformed
+        if (jShowConPassword.isSelected()){
+            jTextConPassword.setEchoChar((char)0);
+        }
+        else{
+            jTextConPassword.setEchoChar('*');
+        }
+    }//GEN-LAST:event_jShowConPasswordActionPerformed
+
+    private void jBResetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBResetActionPerformed
+        jTextName.setText("");
+        jTextEmail.setText("");
+        jTextPassword.setText("");
+        jTextConPassword.setText("");
+    }//GEN-LAST:event_jBResetActionPerformed
+
+    private void RoleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RoleActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_RoleActionPerformed
+
+    private void jBReturnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBReturnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jBReturnActionPerformed
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Register_Page.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Register_Page.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Register_Page.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Register_Page.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Register_Page().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> Role;
+    private javax.swing.JButton jBConfirm;
+    private javax.swing.JButton jBReset;
+    private javax.swing.JButton jBReturn;
+    private javax.swing.JLabel jConPassword;
+    private javax.swing.JLabel jEmail;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jName;
+    private javax.swing.JLabel jPassword;
+    private javax.swing.JCheckBox jShowConPassword;
+    private javax.swing.JCheckBox jShowPassword;
+    private javax.swing.JPasswordField jTextConPassword;
+    private javax.swing.JTextField jTextEmail;
+    private javax.swing.JTextField jTextName;
+    private javax.swing.JPasswordField jTextPassword;
+    // End of variables declaration//GEN-END:variables
+}
